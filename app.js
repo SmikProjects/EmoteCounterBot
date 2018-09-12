44c0265a286cee6c814d347062aa80623bd132e5
Stephen <Stephensjok@hotmail.com>
2018-09-12 11:11:04 +0200

    Changed casing

--- app.js
+++ app.js
@@ -1,36 +1,32 @@
 var tmi = require('tmi.js');
 var i = 0;
-//Put the emote you want to count here
-var emote = "TriHard";
+var emoteText = "TriHard"; //Put the emote you want to count here
+var channelName = "ChannelNameHere"; //Put the channel name here
+var subEmote = true;  //Is the emote a subemote
 
-var options = {
-    options: {
-        debug: true
-    },
+var options =
+{
+    options: {debug: true},
     connection: {
         cluster: "aws",
         reconnect: true
                 },
     identity: {
-        //Bot username
-        username: "UsernameHere",
-        //Bot oauth here
-        password: "oauth:000000000000000000"
-    },
-    //Channels where you want the bot to be active
-    channels: ["ChannelNameHere"]
-    };
+        username: "UsernameHere",             //Bot username
+        password: "oauth:000000000000000000"  //Bot oauth here
+              },
+    channels: [channelName] //Channels where you want the bot to be active
+};
 
 var client = new tmi.client(options);
 client.connect();
 
 client.on('chat', function(channel, user, message, self) {
-          if(message.indexOf(emote) + 1) {
-              while (message.indexOf(emote) + 1){
-              message = message.replace(emote,'')
+          if(message.indexOf(emoteText) + 1) {
+              while (message.indexOf(emoteText) + 1){
+              message = message.replace(emoteText,'')
               i++;
               }
-              //Channel name where the botoutputs the response
-            client.action("ChannelNameHere", "counter " + i);
+            client.action(channelName, "counter " + i); //Channel name where the botoutputs the response
 }
           });
